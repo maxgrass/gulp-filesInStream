dd60f66d0712e2712c17e2ef674bfee9e1548db6
Max Grass <wow64gr@gmail.com>
2014-11-20 10:38:39 +0100

    fixed, checked if first parameter is a function

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ var chalk = require('chalk'),
 const PLUGIN_NAME = 'gulp-filesInStream';
 
 function gulpFilesInStream(printer, eventText) {
-        return mapStream(function (file, cb) {
-            var validPrinter = printer || chalk.white.bold,
-                filepath = path.relative(process.cwd(), file.path);
-            console.log(validPrinter((eventText && (eventText + ' ') || '') + filepath));
+    return mapStream(function (file, cb) {
+        var validPrinter = typeof printer === "function" ? printer : chalk.white.bold,
+            filepath = path.relative(process.cwd(), file.path);
+        console.log(validPrinter((eventText && (eventText + ' ') || '') + filepath));
 
-            return cb(null, file);
-        });
-    };
+        return cb(null, file);
+    });
+};
 
 // exporting the plugin main function
 module.exports = gulpFilesInStream;
